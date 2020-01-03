ff9e641b5163cc80e9303d91e7267cf1aa5546c2
Tony de Faria <tonydefaria@iigethr.com>
2020-01-03 20:30:20 +0000

    added postcss

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 // next.config.js
+
 const withSass = require('@zeit/next-sass')
+
 module.exports = withSass({
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: "[local]__[hash:base64:5]",
-  }
+  cssModules: true
+  // cssLoaderOptions: {
+  //   importLoaders: 1,
+  //   localIdentName: "[local]__[hash:base64:5]",
+  // }
 })
---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+// postcss.config.js
+
+module.exports = {
+  plugins: {
+    'autoprefixer': {},
+    'cssnano': {
+      zindex: false
+    }
+  }
+}
