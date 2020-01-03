e1b5303723e131ef37f2e8a267923d7c2669e219
Tony de Faria <tonydefaria@iigethr.com>
2020-01-03 21:56:09 +0000

    better seo

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,7 @@ const Home = () => (
   <div>
     <Head>
       <title>Tadao Next</title>
-      <meta name="description" content="Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla.">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="description" content="Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla." />
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
