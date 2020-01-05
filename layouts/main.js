98e6a83b74a65bf611f35f751c81269d7817f781
Tony de Faria <tonydefaria@iigethr.com>
2020-01-05 15:28:29 +0000

    added favicon

--- layouts/main.js
+++ layouts/main.js
@@ -12,7 +12,11 @@ export default ({ children }) => (
     <Head>
       <title>Tadao Next</title>
       <meta name="description" content="Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla." />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="favicon/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
+      <link rel="manifest" href="favicon/site.webmanifest" />
     </Head>
     {/* Header */}
     <header className="universal">
