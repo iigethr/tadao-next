6f023302dd6c9d546f65175f97b27089b6aa7bba
Tony de Faria <tonydefaria@iigethr.com>
2020-01-20 09:39:11 +0000

    clean components

--- components/main_menu.js
+++ components/main_menu.js
@@ -2,21 +2,24 @@
 
 import Link from "next/link"
 
-const MainMenu = () => (
-  <div className="main-menu">
-     <div className="main-menu-box">
-        <div className="main-menu-item">
-          <Link href="/">
-            <a className="link-m white mono">Home</a>
-          </Link>
-        </div>
-        <div className="main-menu-item">
-          <Link href="/artboards">
-            <a className="link-m white mono">Artboards</a>
-          </Link>
-        </div>
+class MainMenu extends React.Component {
+
+  render () {
+    <div className="main-menu">
+      <div className="main-menu-box">
+          <div className="main-menu-item">
+            <Link href="/">
+              <a className="link-m white mono">Home</a>
+            </Link>
+          </div>
+          <div className="main-menu-item">
+            <Link href="/artboards">
+              <a className="link-m white mono">Artboards</a>
+            </Link>
+          </div>
+      </div>
     </div>
-  </div>
-)
+  }
+}
 
 export default MainMenu
