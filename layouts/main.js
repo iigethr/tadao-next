5d11c82b92b5ef0554b83ce5b6871150e5390974
Tony de Faria <tonydefaria@iigethr.com>
2020-01-25 17:06:23 +0000

    better fixed headers

--- layouts/main.js
+++ layouts/main.js
@@ -10,7 +10,7 @@ export default (props) => {
   return (
     <div>
       {/* Header */}
-      <header className="universal header fixed-header">
+      <header className="universal header">
         <div className="universal-box">
           <div className="universal-row">
           </div>
