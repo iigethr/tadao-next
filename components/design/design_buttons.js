c37dbb047cfc4d9fe3d01d5492f8c5c47e000142
Tony de Faria <tonydefaria@iigethr.com>
2020-02-03 16:26:52 +0000

    better buttons

--- components/design/design_buttons.js
+++ components/design/design_buttons.js
@@ -7,73 +7,138 @@ class DesignButtons extends React.Component {
       <div className="design-content">
         <div className="design-content-box">
           <div className="design-content-row">
-            <a hreaf="#" className="button-xxl">Button</a>
-          </div>
-        </div>
-        <div className="design-content-box">
-          <div className="design-content-row">
-            <a hreaf="#" className="button-xxl outline">Button</a>
-          </div>
-        </div>
-
-        <div className="design-content-box">
-          <div className="design-content-row">
-            <a hreaf="#" className="button-xxs outline has-icon">
-              <span class="icon-xxs left"><img src={require("../../assets/images/512x512.png")} /></span>
+            <a hreaf="#" className="button-xxl">
               Button
             </a>
-          </div>
-          <div className="design-content-row">
-            <a hreaf="#" className="button-xs outline has-icon">
-              <span class="icon-xs left"><img src={require("../../assets/images/512x512.png")} /></span>
+            <div className="divider-xs" />
+            <a hreaf="#" className="button-xxl condense">
+              <span class="icon-xxl left"><img src={require("../../assets/images/512x512.png")} /></span>
               Button
             </a>
-          </div>
-          <div className="design-content-row">
-            <a hreaf="#" className="button-s outline has-icon">
-              <span class="icon-s left"><img src={require("../../assets/images/512x512.png")} /></span>
+            <div className="divider-xs" />
+            <a hreaf="#" className="button-xxl">
+              <span class="icon-xxl left"><img src={require("../../assets/images/512x512.png")} /></span>
               Button
             </a>
-          </div>
-          <div className="design-content-row">
-            <a hreaf="#" className="button-m outline has-icon">
-              <span class="icon-m left"><img src={require("../../assets/images/512x512.png")} /></span>
+            <div className="divider-xs" />
+            <a hreaf="#" className="button-xxl pill">
               Button
             </a>
-          </div>
-          <div className="design-content-row">
-            <a hreaf="#" className="button-l outline has-icon">
-              <span class="icon-l left"><img src={require("../../assets/images/512x512.png")} /></span>
-              Button
-            </a>
-          </div>
-          <div className="design-content-row">
-            <a hreaf="#" className="button-xl outline has-icon">
-              <span class="icon-xl left"><img src={require("../../assets/images/512x512.png")} /></span>
-              Button
-            </a>
-          </div>
-          <div className="design-content-row">
-            <a hreaf="#" className="button-xxl outline has-icon">
+            <div className="divider-xs" />
+            <a hreaf="#" className="button-xxl pill">
               <span class="icon-xxl left"><img src={require("../../assets/images/512x512.png")} /></span>
               Button
             </a>
           </div>
+          <div className="design-content-row">
+            <a hreaf="#" className="button-xxl outline">
+              Button
+            </a>
+            <div className="divider-xs" />
+            <a hreaf="#" className="button-xxl condense outline">
+              <span class="icon-xxl left"><img src={require("../../assets/images/512x512.png")} /></span>
+              Button
+            </a>
+            <div className="divider-xs" />
+            <a hreaf="#" className="button-xxl outline">
+              <span class="icon-xxl left"><img src={require("../../assets/images/512x512.png")} /></span>
+              Button
+            </a>
+            <div className="divider-xs" />
+            <a hreaf="#" className="button-xxl outline pill">
+              Button
+            </a>
+            <div className="divider-xs" />
+            <a hreaf="#" className="button-xxl outline pill">
+              <span class="icon-xxl left"><img src={require("../../assets/images/512x512.png")} /></span>
+              Button
+            </a>
+          </div>
+          <div className="separator-s" />
+          <div className="design-content-row">
+            <a hreaf="#" className="button-xxl condense">
+              <span class="icon-xxl"><img src={require("../../assets/images/512x512.png")} /></span>
+            </a>
+            <div className="divider-xs" />
+            <a hreaf="#" className="button-xxl">
+              <span class="icon-xxl"><img src={require("../../assets/images/512x512.png")} /></span>
+            </a>
+            <div className="divider-xs" />
+            <a hreaf="#" className="button-xxl pill">
+              <span class="icon-xxl"><img src={require("../../assets/images/512x512.png")} /></span>
+            </a>
+          </div>
+          <div className="design-content-row">
+            <a hreaf="#" className="button-xxl condense outline">
+              <span class="icon-xxl"><img src={require("../../assets/images/512x512.png")} /></span>
+            </a>
+            <div className="divider-xs" />
+            <a hreaf="#" className="button-xxl outline">
+              <span class="icon-xxl"><img src={require("../../assets/images/512x512.png")} /></span>
+            </a>
+            <div className="divider-xs" />
+            <a hreaf="#" className="button-xxl outline pill">
+              <span class="icon-xxl"><img src={require("../../assets/images/512x512.png")} /></span>
+            </a>
+          </div>
         </div>
 
         <div className="design-content-box">
           <div className="design-content-row">
-            <a hreaf="#" className="button-xxl has-icon">
-              <span class="icon-xxl right"><img src={require("../../assets/images/512x512.png")} /></span>
+            <a hreaf="#" className="button-xxs condense">
+              <span class="icon-xxs left"><img src={require("../../assets/images/512x512.png")} /></span>
               Button
             </a>
+            <div className="divider-xxs" />
+            <p className="font-xxs">Button XXS</p>
           </div>
-        </div>
-        <div className="design-content-box">
           <div className="design-content-row">
-            <a hreaf="#" className="button-xxl has-icon">
-              <span class="icon-xxl"><img src={require("../../assets/images/512x512.png")} /></span>
+            <a hreaf="#" className="button-xs condense">
+              <span class="icon-xs left"><img src={require("../../assets/images/512x512.png")} /></span>
+              Button
             </a>
+            <div className="divider-xxs" />
+            <p className="font-xs">Button XS</p>
+          </div>
+          <div className="design-content-row">
+            <a hreaf="#" className="button-s condense">
+              <span class="icon-s left"><img src={require("../../assets/images/512x512.png")} /></span>
+              Button
+            </a>
+            <div className="divider-xxs" />
+            <p className="font-s">Button S</p>
+          </div>
+          <div className="design-content-row">
+            <a hreaf="#" className="button-m condense">
+              <span class="icon-m left"><img src={require("../../assets/images/512x512.png")} /></span>
+              Button
+            </a>
+            <div className="divider-xxs" />
+            <p className="font-m">Button M</p>
+          </div>
+          <div className="design-content-row">
+            <a hreaf="#" className="button-l condense">
+              <span class="icon-l left"><img src={require("../../assets/images/512x512.png")} /></span>
+              Button
+            </a>
+            <div className="divider-xxs" />
+            <p className="font-l">Button L</p>
+          </div>
+          <div className="design-content-row">
+            <a hreaf="#" className="button-xl condense">
+              <span class="icon-xl left"><img src={require("../../assets/images/512x512.png")} /></span>
+              Button
+            </a>
+            <div className="divider-xxs" />
+            <p className="font-xl">Button XL</p>
+          </div>
+          <div className="design-content-row">
+            <a hreaf="#" className="button-xxl condense">
+              <span class="icon-xxl left"><img src={require("../../assets/images/512x512.png")} /></span>
+              Button
+            </a>
+            <div className="divider-xxs" />
+            <p className="font-xxl">Button XXL</p>
           </div>
         </div>
       </div>
