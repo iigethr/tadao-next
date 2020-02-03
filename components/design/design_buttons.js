a304672c47b85678a24a8669c3f5d3d342b56609
Tony de Faria <tonydefaria@iigethr.com>
2020-02-03 12:35:15 +0000

    better buttons

--- components/design/design_buttons.js
+++ components/design/design_buttons.js
@@ -15,7 +15,44 @@ class DesignButtons extends React.Component {
             <a hreaf="#" className="button-xxl outline">Button</a>
           </div>
         </div>
+
         <div className="design-content-box">
+          <div className="design-content-row">
+            <a hreaf="#" className="button-xxs outline has-icon">
+              <span class="icon-xxs left"><img src={require("../../assets/images/512x512.png")} /></span>
+              Button
+            </a>
+          </div>
+          <div className="design-content-row">
+            <a hreaf="#" className="button-xs outline has-icon">
+              <span class="icon-xs left"><img src={require("../../assets/images/512x512.png")} /></span>
+              Button
+            </a>
+          </div>
+          <div className="design-content-row">
+            <a hreaf="#" className="button-s outline has-icon">
+              <span class="icon-s left"><img src={require("../../assets/images/512x512.png")} /></span>
+              Button
+            </a>
+          </div>
+          <div className="design-content-row">
+            <a hreaf="#" className="button-m outline has-icon">
+              <span class="icon-m left"><img src={require("../../assets/images/512x512.png")} /></span>
+              Button
+            </a>
+          </div>
+          <div className="design-content-row">
+            <a hreaf="#" className="button-l outline has-icon">
+              <span class="icon-l left"><img src={require("../../assets/images/512x512.png")} /></span>
+              Button
+            </a>
+          </div>
+          <div className="design-content-row">
+            <a hreaf="#" className="button-xl outline has-icon">
+              <span class="icon-xl left"><img src={require("../../assets/images/512x512.png")} /></span>
+              Button
+            </a>
+          </div>
           <div className="design-content-row">
             <a hreaf="#" className="button-xxl outline has-icon">
               <span class="icon-xxl left"><img src={require("../../assets/images/512x512.png")} /></span>
@@ -23,6 +60,7 @@ class DesignButtons extends React.Component {
             </a>
           </div>
         </div>
+
         <div className="design-content-box">
           <div className="design-content-row">
             <a hreaf="#" className="button-xxl has-icon">
