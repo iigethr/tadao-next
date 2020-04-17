ee2fde16bf99b8e7438845719518b8317b8dfed3
Tony de Faria <tonydefaria@iigethr.com>
2020-04-17 19:20:42 +0100

    better campaigns

--- pages/[lang]/campaigns/index.js
+++ pages/[lang]/campaigns/index.js
@@ -2,12 +2,12 @@
 
 // Components
 import Head from "next/head"
-import Notifications from "../../lib/globals/notifications"
-import Header from "../../lib/globals/header"
-import Footer from "../../lib/globals/footer"
-import Hero from "../../lib/components/campaigns/hero"
+import Notifications from "../../../lib/globals/notifications"
+import Header from "../../../lib/globals/header"
+import Footer from "../../../lib/globals/footer"
+import Hero from "../../../lib/components/campaigns/hero"
 // Data
-import data from "js-yaml-loader!../../data/campaigns/campaigns.yml"
+import data from "js-yaml-loader!../../../data/campaigns/campaigns.yml"
 
 class Index extends React.Component {
   static async getInitialProps() {
@@ -35,6 +35,38 @@ class Index extends React.Component {
               <div className="universal-row">
                 <div className="artboard-l center-h">
                   <Hero campaigns={campaigns} />
+                  <form className="form white-bg" id="form" action="https://submit-form.com/dVlr2taOeSnCytPgDKqVC" target="_self">
+                    <input type="checkbox" name="_honeypot" tabIndex="-1" autoComplete="off" className="honeypot" />
+                    <fieldset>
+                      <legend>Data Submission with GDPR Consentment Box</legend>
+                      <div className="form-row">
+                        <div className="form-item">
+                          <label>Email</label>
+                          <div className="input">
+                            <input type="email" name="email" autoComplete="off" required />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="form-row">
+                        <div className="form-item">
+                          <div className="submit">
+                            <input value="Submit" type="submit" className="button-xl blue wide" />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="form-row">
+                        <div className="form-item">
+                          <div className="gdpr">
+                            <label htmlFor="checkbox">
+                              <input type="checkbox" id="checkbox" name="gdpr" value="consent" />
+                              <span className="checkmark"></span>
+                              I agree to ... donec id elit non mi porta gravida at eget metus. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Vestibulum id ligula porta felis euismod semper.
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+                    </fieldset>
+                  </form>
                 </div>
               </div>
             </div>
--- pages/_app.js
+++ pages/_app.js
@@ -23,7 +23,7 @@ class MyApp extends App {
 
   render() {
     const { Component, pageProps, router, project } = this.props
-    if (router.pathname.startsWith("/campaigns")) {
+    if (router.pathname.startsWith("/[lang]/campaigns")) {
       return (
         <Campaigns>
           <Component {...pageProps} {...project} />
